7db8f06a9f8c6d648564c0f242398a107b9be401
Philip Vollmann <Philip.vollmann0408@gmail.com>
2023-10-24 16:27:08 +0200

    implemented input logic/ it is now possible to search for citys and get autocomplete suggestions

--- Frontend/Weather-App-Frontend/Pages/SearchWeatherAfterLocation.jsx
+++ Frontend/Weather-App-Frontend/Pages/SearchWeatherAfterLocation.jsx
@@ -1,13 +1,33 @@
 import React, { useState } from 'react';
-
+import SearchCity from '../Components/SearchCity';
 
 const SearchWeatherAfterLocation = () => {
     const [autoComplete,setAutoComplete] = useState("");
+    const [weatherData,setWeatherData] = useState([]);
+
+
+    
+
 
     return(
         <div>
             <h1>Enter a City to get the Weather Details</h1>
-            <input type="text"></input>
+            <input type="text"
+                   onChange={e => {
+                    const newValue = e.target.value;
+                    setAutoComplete(newValue);
+                    SearchCity(newValue, setWeatherData);
+                }}></input>
+                    <div>
+    {weatherData.map((data, index) => (
+        <div key={index}>
+            <div>Name: {data.name},{data.country}</div>
+            
+        </div>
+    ))}
+</div>
+
+
         </div>
     )
 }
