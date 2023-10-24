b7a9668f36c7c4523abe534670e31d9c7f0f6554
Philip Vollmann <Philip.vollmann0408@gmail.com>
2023-10-24 10:46:16 +0200

    changed weatherData to match the json format from the Api call

--- Backend/Weather-App-Backend/src/main/java/com/vollphil/WeatherAppBackend/WeatherData.java
+++ Backend/Weather-App-Backend/src/main/java/com/vollphil/WeatherAppBackend/WeatherData.java
@@ -1,31 +1,185 @@
 package com.vollphil.WeatherAppBackend;
 
 public class WeatherData {
-    private String condition;
-    private double temperature;
+  private  Location location;
+  private Current current;
 
-    public WeatherData(String condition, double temperature) {
-        this.condition = condition;
-        this.temperature = temperature;
+    public Location getLocation() {
+        return location;
     }
 
-    public WeatherData() {
+    public void setLocation(Location location) {
+        this.location = location;
     }
 
-    public String getCondition() {
+    public Current getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(Current current) {
+        this.current = current;
+    }
+
+    public static class Location{
+    private String name;
+    private String region;
+    private String country;
+    private Double lat;
+    private Double lon;
+    private String tz_id;
+    private Long localtime_epoch;
+    private String localtime;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getRegion() {
+            return region;
+        }
+
+        public void setRegion(String region) {
+            this.region = region;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public Double getLat() {
+            return lat;
+        }
+
+        public void setLat(Double lat) {
+            this.lat = lat;
+        }
+
+        public Double getLon() {
+            return lon;
+        }
+
+        public void setLon(Double lon) {
+            this.lon = lon;
+        }
+
+        public String getTz_id() {
+            return tz_id;
+        }
+
+        public void setTz_id(String tz_id) {
+            this.tz_id = tz_id;
+        }
+
+        public Long getLocaltime_epoch() {
+            return localtime_epoch;
+        }
+
+        public void setLocaltime_epoch(Long localtime_epoch) {
+            this.localtime_epoch = localtime_epoch;
+        }
+
+        public String getLocaltime() {
+            return localtime;
+        }
+
+        public void setLocaltime(String localtime) {
+            this.localtime = localtime;
+        }
+    }
+
+public static class Current{
+    private Long last_updated_epoch;
+    private String last_updated;
+    private Double temp_c;
+    private Double temp_f;
+    private Integer is_day;
+    private Condition condition;
+
+    public Long getLast_updated_epoch() {
+        return last_updated_epoch;
+    }
+
+    public void setLast_updated_epoch(Long last_updated_epoch) {
+        this.last_updated_epoch = last_updated_epoch;
+    }
+
+    public String getLast_updated() {
+        return last_updated;
+    }
+
+    public void setLast_updated(String last_updated) {
+        this.last_updated = last_updated;
+    }
+
+    public Double getTemp_c() {
+        return temp_c;
+    }
+
+    public void setTemp_c(Double temp_c) {
+        this.temp_c = temp_c;
+    }
+
+    public Double getTemp_f() {
+        return temp_f;
+    }
+
+    public void setTemp_f(Double temp_f) {
+        this.temp_f = temp_f;
+    }
+
+    public Integer getIs_day() {
+        return is_day;
+    }
+
+    public void setIs_day(Integer is_day) {
+        this.is_day = is_day;
+    }
+
+    public Condition getCondition() {
         return condition;
     }
 
-    public void setCondition(String condition) {
+    public void setCondition(Condition condition) {
         this.condition = condition;
     }
 
-    public double getTemperature() {
-        return temperature;
-    }
+    public  static class Condition{
+        private String text;
+        private String icon;
+        private Integer code;
 
-    public void setTemperature(double temperature) {
-        this.temperature = temperature;
-    }
+        public String getText() {
+            return text;
+        }
 
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public String getIcon() {
+            return icon;
+        }
+
+        public void setIcon(String icon) {
+            this.icon = icon;
+        }
+
+        public Integer getCode() {
+            return code;
+        }
+
+        public void setCode(Integer code) {
+            this.code = code;
+        }
+    }
 }
+}
+
