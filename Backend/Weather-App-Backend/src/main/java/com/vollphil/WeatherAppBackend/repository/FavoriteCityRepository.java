0f845959d27d4b92f0f4bba228e0c8f829c7b656
Philip Vollmann <Philip.vollmann0408@gmail.com>
2023-10-26 15:31:26 +0200

    added H2 config to the application properties/ added annotation to FavoriteCityRepository /

--- Backend/Weather-App-Backend/src/main/java/com/vollphil/WeatherAppBackend/repository/FavoriteCityRepository.java
+++ Backend/Weather-App-Backend/src/main/java/com/vollphil/WeatherAppBackend/repository/FavoriteCityRepository.java
@@ -2,6 +2,8 @@ package com.vollphil.WeatherAppBackend.repository;
 
 import com.vollphil.WeatherAppBackend.data.FavoriteCity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FavoriteCityRepository extends JpaRepository<FavoriteCity,Long> {
 }
